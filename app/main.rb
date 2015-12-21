d7f63525a51941f9c52861ee3da83c0ff6676984
Code DIvision <codedivision@Codes-Mac-mini.local>
2015-12-21 20:16:03 +0800

    left order desc -2 part

--- app/main.rb
+++ app/main.rb
@@ -33,14 +33,49 @@ class Queries
     end
 
   def self.active
+    array = []
+    Legislator.all.each do |legislator|
+      @sen = Legislator.where(in_office: 1, state: legislator.state, title: "Sen").count
+      # @sen1 = @sen.order('count_in_office desc')
+      @rep = Legislator.where(in_office: 1, state: legislator.state, title: "Rep").count
+      i = "#{legislator.state}:  #{@sen} Senators, #{@rep} Representative(s)"
+      array << i.split(" ")
+    end
 
-    Legislator.last_name_p(state)
 
+    array.each_with_index do |x, index|
+
+      array[index][3] <=> array[index][3]
+    end
+    p array
+
+  end
+
+
+  def total
+    @sen = Legislator.where(title: "Sen").count(:in_office)
+    @rep = Legislator.where(title: "Rep").count(:in_office)
+    puts " Senators: #{@sen}"
+    puts " Representatives: #{@rep}"
+  end
+
+  def self.delete_out
+    sen = Legislator.where(title: "Sen", in_office: 0)
+    rep = Legislator.where(title: "Rep", in_office: 0)
+    sen1 = Legislator.where(title: "Sen", in_office: 1)
+    rep1 = Legislator.where(title: "Rep", in_office: 1)
+    Legislator.delete(sen)
+    Legislator.delete(rep)
+
+    puts "Senators :#{sen1.count}"
+    puts "Representatives :#{rep1.count}"
 
   end
   # def senator(state)
   #   @sen = Legistator.selet(:in_office).count.where(title: "Sen"  state: state)
   # end
+
+  Queries.delete_out
 
 
 
